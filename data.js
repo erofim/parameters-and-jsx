14a29ced2e0cbd10b936fa606a9b08e05684b85f
erofim <ver.kab@mail.ru>
2023-10-04 21:59:49 +0300

    Заполняет шаблон из внешних данных

---
+++ data.js
@@ -0,0 +1,13 @@
+const cardData = {
+  name: 'Катер Meridian 401',
+  description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+  img: 'img/photo-card-1.jpg',
+  price: 14000,
+  features: [
+    '2 каюты',
+    '4 спальных места',
+    'душ, туалет, джакузи',
+  ]
+};
+
+window.cardData = cardData;
